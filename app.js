d5e10368296ef47a29ac34b71f00c39a7a6d1512
NikitaGe <nikitage2002@gmail.com>
2024-02-21 08:56:09 +0100

    push

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const UserController = require('./controller/users_conn')
-
+app.use(cors());
 
 app.use('/api/users', UserController)
 
