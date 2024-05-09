be5038bfbbc408fd80e60d43100e9d6a076fd608
NikitaGe <nikitage2002@gmail.com>
2024-05-09 13:05:32 +0200

    ngk push

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const UserController = require('./controller/users_conn')
+
+app.use(express.json());
+app.use(session({
+  name : 'codeil',
+    secret : 'secret_session_tgbbz1',
+    resave :false,
+    saveUninitialized: false,
+    cookie : {maxAge:(1000 * 60 * 100)}      
+
+}))
 app.use(cors());
-
 app.use('/api/users', UserController)
-
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 })
--- controller/users_conn.js
+++ controller/users_conn.js
@@ -1,10 +1,26 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const UserServices = require('../services/users_serv')
-
-
+const UserServices = require('../services/users_serv');
 
 //Routes
+
+router.post('/login', async (req, res) => {
+  //console.log("login", req.body);
+
+  console.log("login", req.body);
+
+
+
+   
+
+})
+
+
+
+
+
+
+
 router.get('/getUserList', async (req, res)=> {
     try {
       const data = await UserServices.getUsers(1);
@@ -20,21 +36,23 @@ router.get('/getUserList', async (req, res)=> {
       let idclass = req.query.idclass
       const data = await UserServices.getUsersofClass(idclass);
       res.send(data)
-    }catch(err) {
+    } catch(err) {
       console.log(err);
       res.status(400)
     }
   })
   
-  router.get('/getClasses', async (req, res)=> {
+  router.get('/getClasses', async (req, res) => {
     try {
       const data = await UserServices.getClass();
       res.send(data)
-    }catch(err) {
-      console.log(err);
+    } catch(err) {
+        console.log(err);
       res.status(400)
       }
   })
+
+
   
 
 
